c330cc7b31438f5a14fb0edd38b88a599b442c05
loic <loic.hermann@free.fr>
2018-12-08 16:12:23 +0100

    allow to go backward with cd

--- lib/Bin/Cd.php
+++ lib/Bin/Cd.php
@@ -14,18 +14,39 @@ class Cd extends BinBase {
       $currentView->chroot($this->shell->getHomeView()->getRoot());
       return;
     }
-    if(!$currentView->file_exists($cmd->getArg(1))){
+
+    $currentLocationArray = explode(  "/" , $this->shell->getHomeView()->getRelativePath($currentView->getRoot()));
+
+    if(end($currentLocationArray) === "" ){
+        array_pop ( $currentLocationArray );
+    }
+    var_dump($currentLocationArray);
+
+    $relativeTargetLocationArray = explode("/", $cmd->getArg(1));
+
+    foreach($relativeTargetLocationArray as $item){
+      if($item ===".."){
+        array_pop ( $currentLocationArray );
+      }else{
+        array_push ($currentLocationArray, $item) ;
+      }
+    }
+    var_dump($currentLocationArray);
+
+    $targetLocation = implode("/", $currentLocationArray);
+
+    if(!$this->shell->getHomeView()->file_exists($targetLocation)){
       $output->writeln($cmd->getArg(1).": No such file or directory");
       return;
     }
-    if(!$currentView->is_dir($cmd->getArg(1))){
+    if(!$this->shell->getHomeView()->is_dir($targetLocation)){
       $output->writeln($cmd->getArg(1).": Not a directory");
       return;
     }
 
     //[TODO] Still need to handle ".." in path
 
-    $currentView->chroot($currentView->getRoot().'/'.$cmd->getArg(1));
+    $currentView->chroot($this->shell->getHomeView()->getRoot().$targetLocation);
 
 
   }
